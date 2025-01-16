9190cec2d205094f0cdba33de417920ba0091cc5
cheongyeechian <cheongyeechian@gmail.com>
2025-01-16 16:18:04 +0800

    fix

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,8 +1,8 @@
 // import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import {db} from "./connect"
-import { NextAuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { db } from "./connect";
+import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -13,8 +13,10 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET || "",
     }),
   ],
-  secret: process.env.SECRET,
   session: {
     strategy: "jwt",
-  }
-}
+  },
+  pages: {
+    signIn: "/loginPage",
+  },
+};
